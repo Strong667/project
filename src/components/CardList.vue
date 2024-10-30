14eeb8bd3575cb6879a5a59dc4ec34162e947054
@alikhan <aahmetov755@gmail.com>
2024-10-30 18:13:36 +0500

    first

---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские Кроссовки Nike Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :isAdded="true"
+      :isFavorite="true"
+      :onClickAdd="onClickAdd"
+      :onClickFavorite="onClickFavorite"
+    />
+  </div>
+</template>
+
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Done!')
+}
+
+const onClickFavorite = () => {
+  alert('Done2!')
+}
+</script>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <DrawerHead />
+    <CartItemList />
+    <div>
+      <div>
+        <span> Итого: </span>
+        <div></div>
+        <b>12990 ₽</b>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CartItemList from './CartItemList.vue'
+import DrawerHead from './DrawerHead.vue'
+</script>
