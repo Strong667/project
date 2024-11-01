9c428e78af88bd5f615be95a2d97524bdfd227f9
@alikhan <aahmetov755@gmail.com>
2024-11-01 17:23:04 +0500

    proverka2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -3,12 +3,25 @@
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
     <CartItemList />
-    <div>
-      <div>
+    <div class="flex flex-col gap-4 mt-7">
+      <div class="flex gap-2">
         <span> Итого: </span>
-        <div></div>
-        <b>12995 ₽</b>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12990 ₽</b>
       </div>
+
+      <div class="flex gap-2">
+        <span> Налог 5%: </span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>900 ₽</b>
+      </div>
+
+      <button
+        disabled=""
+        class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+      >
+        Оформить заказ
+      </button>
     </div>
   </div>
 </template>
